43a6395ec5d6c6103f815d23ef04c22500513726
alexdavis10 <alexd43@gmail.com>
2022-09-28 13:35:31 -0500

    Update to code

--- New Hire/New_ACOM_User.ps1
+++ New Hire/New_ACOM_User.ps1
@@ -1,9 +1,7 @@
 Add-Type -AssemblyName System.Web
 Import-Csv -Path C:\New_Hire.csv | foreach {
-    $password = [System.Web.Security.Membership]::GeneratePassword((Get-Random -Minimum 20 -Maximum 32), 3)
-    $secPw = ConvertTo-SecureString -String $password -AsPlainText -Force
-
-	$userName = '{0}{1}' -f $_.FirstName.Substring(0,1),$_.LastName
+    
+	$userName = $_.FirstName + " " + $_.LastName
 	$NewUserParameters = @{
 		GivenName = $_.FirstName
 		Surname = $_.LastName
