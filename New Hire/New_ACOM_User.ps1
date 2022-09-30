cb32ef56c8725c735292a319f42ba2d3441170d5
alexdavis10 <alexd43@gmail.com>
2022-09-30 09:16:13 -0500

    Updated new user script and added comments.

--- New Hire/New_ACOM_User.ps1
+++ New Hire/New_ACOM_User.ps1
@@ -1,24 +1,35 @@
 Add-Type -AssemblyName System.Web
-Import-Csv -Path C:\New_Hire.csv | foreach {
-    
-	$userName = $_.FirstName + " " + $_.LastName
+
+# Parameters to extract from New_Hire.csv file
+Import-Csv -Path $CSVPath | foreach {
+   
 	$NewUserParameters = @{
 		GivenName = $_.FirstName
 		Surname = $_.LastName
 		sAMAccountName = $_.UserName
-        mail = $_.EmailAddress
-        title = $_.Title
-        descriptin = $_.Title
-        department = $_.Department
-        proxyAddresses = "SMTP:$_.EmailAddress"
-        targetAddress = $_.UserName + "@acomedu.mail.onmicrosoft.com"
-        AccountPassword = $secPw
+        userPrincipalName = $_.EmailAddress
+        EmailAddress = $_.EmailAddress
+        Title = $_.Title
+        Description = $_.Title
+        Department = $_.Department
+        employeeNumber = $_.EmployeeNumber        
 	}
-	New-AdUser @NewUserParameters
 
+    # Enter location of New_Hire.csv file where new hire info is entered.
+    $CSVPath = Read-Host "Location of New_Hire.csv: "
+
+    #Path to OU
+    $OUPath = Read-Host "Path to OU. Example (OU=Faculty,DC=acom,DC=edu)"
+
+    # Creating and setting attributes for new user.
+	New-ADUser @NewUserParameters -Name "$($_.FirstName + " " + $_.LastName)" -Path $OUPath -Enabled $true -AccountPassword (Read-Host -AsSecureString "Input User Passowrd")
+    Set-ADUser -Identity $_.UserName -Add @{targetAddress = $_.UserName + "@acomedu.mail.onmicrosoft.com";ProxyAddresses = "SMTP:" + $_.EmailAddress}
+
+    # Retrieving all available groups to add user to.
     $groups =  Get-ADgroup -Filter * | Select-Object -Property Name| Out-GridView -PassThru -Title "Select the Groups you want to add the users" | Select-Object -ExpandProperty Name
 
+    # Add user to selected groups.
     foreach($group in $groups){
-        Add-ADGroupMember -Identity $group -Members $$_.UserName
+        Add-ADGroupMember -Identity $group -Members $_.UserName
     }
 }
