0bc45aa558043f1721fa9adcd3df551ce56a9af5
alexdavis10 <alexd43@gmail.com>
2022-09-30 09:25:47 -0500

    Add 365 license script and csv sample.

---
+++ 365LicenseUpdate/Update365LicenseScript.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module MSOnline
+Connect-MsolService
+
+$users = import-csv "C:\Users\adavis\OneDrive - Alabama College of Osteopathic Medicine\Desktop\MyPowerShellScripts\365LicenseUpdater.csv" -Delimiter ","
+foreach ($user in $users)
+{
+    $upn=$user.UserPrincipalName
+    $usagelocation=$user.usagelocation
+    $SKU=$user.SKU
+    Set-MsolUser -UserPrincipalName $upn -UsageLocation $usagelocation
+    Set-MsolUserLicense -UserPrincipalName $upn -AddLicenses $SKU
+}
