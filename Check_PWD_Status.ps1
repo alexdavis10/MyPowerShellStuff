6a07f40e83d298c5649dc0fa34c4da9518334264
alexdavis10 <alexd43@gmail.com>
2022-10-10 09:22:23 -0500

    Add check Lockout status ps1 script

---
+++ Check_PWD_Status.ps1
@@ -0,0 +1,5 @@
+﻿## Script to check lockout status and last password set date.
+
+$Username = Read-Host "Username to check: "
+
+Get-ADUser -Identity $Username -Properties * | ft name,passwordlastset,passwordexpired,passwordneverexpires
