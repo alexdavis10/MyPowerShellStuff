42d6ce79d46fcf3fc2c4efa694e14bc1fdcf3ac4
alexdavis10 <alexd43@gmail.com>
2022-10-17 11:22:01 -0500

    moved vcenter script to local

--- vCenter Connect.ps1
+++
@@ -1,7 +0,0 @@
-﻿### Script to connec to both vcenter's
-
-$vCenterCred = Get-Credential
-
-$vCenterHost = Read-Host "vCenter(s) you wish to connect to: "
-
-Connect-VIServer $vCenterHost -credential $vCenterCred
